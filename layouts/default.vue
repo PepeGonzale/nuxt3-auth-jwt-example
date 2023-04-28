c5fe550d069cad379901b1a0d1d36fa73a17797a
Pepe <94927052+PepeGonzale@users.noreply.github.com>
2023-04-28 17:18:17 +0200

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="min-h-screen flex flex-col bg-slate-900">
+    <header class=" p-3 mx-auto w-full max-w-4xl">
+        <nav class="flex gap-3 text-white">
+            <PageLink to="/" class="underline">Home</PageLink>
+            <PageLink to="/" class="underline">Public</PageLink>
+            <PageLink to="/" class="underline hidden">Private</PageLink>
+            <PageLink to="/admin" class="underline hidden">Admin</PageLink>
+            <button class="ml-auto text-black py-1 px-2 rounded bg-light-100 hover bg-white">
+            Login
+        </button>
+        </nav>
+       
+    </header>
+    <main p-3 mx-auto w-full max-w-4xl>
+      <slot />
+    </main>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="text-white">
+    <div class="text-center">
+        <h1>Home</h1>
+        <div>
+            Simple authentication with Nuxt3
+            <span>current user: </span>
+        </div>
+    </div>
+</div>
+</template>
