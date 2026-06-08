adf5bdf59597704a2ba87adf5d928513763f12ad
PepeGonzale <pepeglezdevega@gmail.com>
2026-06-08 15:46:07 +0200

    feat: add SVG favicon with Sentinel shield design

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,16 @@ export default defineNuxtConfig({
     head: {
       link: [
         {
+          rel: 'icon',
+          type: 'image/svg+xml',
+          href: '/favicon.svg'
+        },
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico'
+        },
+        {
           rel: 'preconnect',
           href: 'https://fonts.googleapis.com'
         },
